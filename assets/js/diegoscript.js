cb2ad442e0264677068f9f281ec1dbfa7cc86c60
Diego Rivera <93798677+Diegorivera1110@users.noreply.github.com>
2022-04-30 15:35:35 -0700

    created search function

--- assets/js/diegoscript.js
+++ assets/js/diegoscript.js
@@ -1,16 +1,59 @@
-var animeSearchEl = document.querySelector("#search-anim");
+var animeSearchEl = document.querySelector("#search-anime");
+var animeTitleInput = document.querySelector("#anime-search-input")
+var mangaCheck = false;
 
 
-var searchAnime = function() {
+var searchAnime = function(anime) {
+  console.log(anime);
+
     
-  var apiUrl = "https://api.jikan.moe/v3/search/anime?q=&order_by=members&sort=desc&page=1";
+  // var apiUrl = "https://api.jikan.moe/v3/search/anime?q=" + title + "&order_by=members&sort=desc&page=1";
+  var apiUrl =  "https://api.jikan.moe/v3/search/anime?q=" + anime + "&page=1"
 
   fetch(apiUrl).then(function(response) {
     if(response.ok){
         response.json().then(function(data){
             console.log(data);
+            // console.log(data.results[0].title)
+            // console.log(data.results[0].synopsis)
+            // console.log(data.results[0].image_url)
+            $("#noAnime").addClass("display-off");
 
-            var animeTitle = document.createElement
+            $("#animeTitle").text(data.results[0].title);
+            $("#synopsis").text(data.results[0].synopsis);
+            $("#animeImg").attr("src", data.results[0].image_url);
+
+            gotManga(data.results[0].title);
+            
+           
+          })
+        } else {
+          $("#animeTitle").text("");
+          $("#synopsis").text("");
+          $("#animeImg").attr("src", "");
+
+          $("#manga").text("");
+
+          $("#noAnime").removeClass("display-off");
+
+        }
+      })
+    };
+    
+    
+    function gotManga(name) {
+      
+      var apiUrl =  "https://api.jikan.moe/v3/search/manga?q=" + name + "&page=1"
+      
+      fetch(apiUrl).then(function(response) {
+        if(response.ok){
+          response.json().then(function(data){
+            
+            if (data.results.length > 0) {
+              $("#manga").text("Has a manga")
+            } else {
+              $("#manga").text("Does not have Manga");
+            }
 
 
         })
@@ -18,4 +61,22 @@ var searchAnime = function() {
   })
   };
 
-searchAnime();
+
+  // document.addEventListener("DOMContentLoaded", function(){
+  //   var elems = document.querySelectorAll('#modal1');
+  //   var instances = M.Modal.init(elems, onOpenStart)
+  //   console.log()
+  // })
+
+  $(document).ready(function(){
+    $('.modal').modal();
+  });
+
+
+  animeSearchEl.addEventListener("click", function() {
+      console.log("click");
+
+      searchAnime(animeTitleInput.value);
+  })
+
+// searchAnime();
